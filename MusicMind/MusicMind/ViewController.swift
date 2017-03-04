d6104ea6f85a58914f39df3af17a1b9dd1053f9e
Wes Van der Klomp <wes.vdk@icloud.com>
2017-03-03 16:44:50 -0800

    Connect UI to VC and attempt login - Closes #9

--- MusicMind/MusicMind/ViewController.swift
+++ MusicMind/MusicMind/ViewController.swift
@@ -7,9 +7,25 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    // TODO: Disable button if email || password fields are nil. Use textFieldDelegate to check everytime keypress.
+    
+    @IBAction func attemptSignIn(_ sender: Any) {
+        FIRAuth.auth()?.signIn(withEmail: emailField.text!, password: passwordField.text!, completion: { (user, error) in
+            if error == nil {
+                self.view.backgroundColor = .green
+            } else {
+                self.view.backgroundColor = .red
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
