df4bdb704b70c923abae6e406bceaf8fabf3f886
Angel Contreras <angel.contreras23@me.com>
2017-03-17 16:58:50 -0700

    added emoji images

---
+++ MusicMind/MusicMind/CameraOverlayViewController + collectionViewHelper.swift
@@ -0,0 +1,13 @@
+//
+//  CameraOverlayViewController + collectionViewHelper.swift
+//  MusicMind
+//
+//  Created by Angel Contreras on 3/17/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import UIKit
+
+extension CameraOverlayViewController: UICollectionViewDelegate, UICollectionViewDatasource {
+    
+}
--- MusicMind/MusicMind/CameraOverlayViewController/CameraOverlayViewController.swift
+++ MusicMind/MusicMind/CameraOverlayViewController/CameraOverlayViewController.swift
@@ -33,3 +33,4 @@ class CameraOverlayViewController: UIViewController {
     */
 
 }
+
