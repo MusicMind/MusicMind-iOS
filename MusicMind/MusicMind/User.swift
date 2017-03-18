a58c1f033236cda01aff89c60ead1b4770844054
Wes Van der Klomp <wes.vdk@icloud.com>
2017-03-18 10:13:47 -0700

    create User.swift

---
+++ MusicMind/MusicMind/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  MusicMind
+//
+//  Created by Wesley Van der Klomp on 3/18/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import UIKit
+import KeychainSwift
+
+class User: NSObject {
+
+}
