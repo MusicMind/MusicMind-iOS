f189245936e5feebc620fa70ede0bf68aa3d17ca
Wes Van der Klomp <wes.vdk@icloud.com>
2017-03-17 21:03:53 -0700

    properly rename ViewController to SignInViewController

--- MusicMind/MusicMind/SignIn/SignInViewController.swift
+++ MusicMind/MusicMind/SignIn/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  MusicMind
 //
 //  Created by Wesley Van der Klomp on 2/28/17.
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
@@ -35,24 +35,6 @@ class ViewController: UIViewController {
             }
         })
     }
-    
-//    @IBAction func attemptSignUp(_ sender: Any) {
-//        FIRAuth.auth()?.createUser(withEmail: emailField.text!, password: passwordField.text!, completion: { (user, error) in
-//            if error == nil {
-//                self.view.backgroundColor = .blue
-//                
-//                print(user?.uid)
-//                
-//            } else {
-//                self.view.backgroundColor = UIColor.brown
-//            }
-//        })
-//    }
-    
-//    @IBAction func showCameraButtonPressed(_ sender: Any) {
-//        // Show the camera view controller
-//
-//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
