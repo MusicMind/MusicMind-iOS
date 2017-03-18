33fb731cf37bbad0255083f7753fed071ec9681d
Wes Van der Klomp <wes.vdk@icloud.com>
2017-03-18 10:22:23 -0700

    basic keychain stuff in user class

--- MusicMind/MusicMind/User.swift
+++ MusicMind/MusicMind/User.swift
@@ -10,5 +10,33 @@ import UIKit
 import KeychainSwift
 
 class User: NSObject {
+    
+    private let keychain: KeychainSwift!
+    
+    /// Constant keys for accessing values in keychain
+    private enum KeychainKey: String {
+        case firebaseUserEmail = "MMEmail"
+        case firebaseUserPassword = "MMPassword"
+    }
+    
+    override init() {
+        keychain = KeychainSwift()
+
+        super.init()
+    }
+
+    var firebaseUserEmail: String? {
+        get {
+            
+            return keychain.get(KeychainKey.firebaseUserEmail.rawValue)
+        }
+        set {            
+            if let unwrappedNewValue = newValue {
+                keychain.set(unwrappedNewValue, forKey: KeychainKey.firebaseUserEmail.rawValue)
+            } else {
+                keychain.delete(KeychainKey.firebaseUserEmail.rawValue)
+            }
+        }
+    }
 
 }
