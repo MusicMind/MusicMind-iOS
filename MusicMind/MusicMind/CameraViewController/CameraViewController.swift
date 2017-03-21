5fe84f15081b9753bb8222283d65ed4f14a558e5
Wes Van der Klomp <wes.vdk@icloud.com>
2017-03-21 12:04:03 -0700

    fix storyboard name in vc loading

--- MusicMind/MusicMind/CameraViewController/CameraViewController.swift
+++ MusicMind/MusicMind/CameraViewController/CameraViewController.swift
@@ -84,7 +84,7 @@ extension CameraViewController: APPLCameraViewControllerDelegate {
     // MARK: - Post to firebase
     func didFinishRecordingToOutputFileAt(outputUrl: URL) {
         // Show the camera view controller
-        let sendToFriendViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SendToFriend") as! SendToFriendViewController
+        let sendToFriendViewController = UIStoryboard(name: "SendToFriend", bundle: nil).instantiateViewController(withIdentifier: "SendToFriend") as! SendToFriendViewController
         
         sendToFriendViewController.urlOfVideo = outputUrl
         
