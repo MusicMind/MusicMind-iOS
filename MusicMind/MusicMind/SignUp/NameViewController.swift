13606164dfbcd9ccfedbc691f2dd8d324fe540ec
Angel Contreras <angel.contreras23@me.com>
2017-03-28 17:34:06 -0700

    NameViewController class

---
+++ MusicMind/MusicMind/SignUp/NameViewController.swift
@@ -0,0 +1,41 @@
+//
+//  NameViewController.swift
+//  MusicMind
+//
+//  Created by Angel Contreras on 3/28/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import UIKit
+
+class NameViewController: UITabBarController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signUpButtonPressed(_ sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
