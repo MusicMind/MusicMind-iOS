88e71556b27ce8c77e0bd351a5f9fab2f446d063
Angel Contreras <angel.contreras23@me.com>
2017-04-04 11:21:27 -0700

    User struct

--- MusicMind/MusicMind/User.swift
+++ MusicMind/MusicMind/User.swift
@@ -10,7 +10,46 @@ import Foundation
 
 struct User{
     
-    let firstName: String
-    let lastName: String
-    let birthday: Date
+    private enum UserDefaultsKeys: String{
+        case firstNameKey = "MMFirstName"
+        case lastNameKey = "MMLastName"
+        case birthdayKey = "MMBirthday"
+        case firebaseUUIDKey = "MMFirbaseUUID"
+    }
+    
+    var firstName: String?{
+        get{
+            return UserDefaults.standard.object(forKey: UserDefaultsKeys.firstNameKey.rawValue) as? String
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.firstNameKey.rawValue)
+        }
+    }
+    
+    var lastName: String?{
+        get{
+            return UserDefaults.standard.object(forKey: UserDefaultsKeys.lastNameKey.rawValue) as? String
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.lastNameKey.rawValue)
+        }
+    }
+    
+    var birthday: Date?{
+        get{
+            return UserDefaults.standard.object(forKey: UserDefaultsKeys.birthdayKey.rawValue) as? Date
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.birthdayKey.rawValue)
+        }
+    }
+    
+    var firebaseUUID: String? {
+        get{
+            return UserDefaults.standard.object(forKey: UserDefaultsKeys.firebaseUUIDKey.rawValue) as? String
+        }
+        set {
+            UserDefaults.standard.set(newValue, forKey: UserDefaultsKeys.firebaseUUIDKey.rawValue)
+        }
+    }
 }
