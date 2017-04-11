31e3e1af9a268163cbe21581ab18f04b78ef832d
Angel Contreras <angel.contreras23@me.com>
2017-04-10 17:15:56 -0700

    email verifier

---
+++ MusicMind/MusicMind/EmailVerifier.swift
@@ -0,0 +1,15 @@
+//
+//  EmailVerifier.swift
+//  MusicMind
+//
+//  Created by Angel Contreras on 4/10/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import Foundation
+
+class EmailVerifier{
+    static func isValid(email: String) -> Bool {
+        
+    }
+}
