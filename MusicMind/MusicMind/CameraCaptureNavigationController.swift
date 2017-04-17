edc2b55a6cd443aaa8c59f0cc20cd01761870f40
Wes Van der Klomp <wes.vdk@icloud.com>
2017-04-17 12:08:39 -0700

    move methods over

--- MusicMind/MusicMind/CameraCaptureNavigationController.swift
+++ MusicMind/MusicMind/CameraCaptureNavigationController.swift
@@ -20,7 +20,7 @@ class CameraCaptureNavigationController: UINavigationController {
         super.viewDidLoad()
         
         // Setup gesture recognizer
-        edgeGesture = UIScreenEdgePanGestureRecognizer(target: interactionController, action: #selector(edgeGestureAction(sender:)))
+        edgeGesture = UIScreenEdgePanGestureRecognizer(target: interactionController, action: #selector(NavigateLeftTransitionInteractionController.edgeGestureAction(sender:)))
         
         if let edgeGesture = edgeGesture {
             edgeGesture.edges = UIRectEdge.left
@@ -30,76 +30,6 @@ class CameraCaptureNavigationController: UINavigationController {
         
         delegate = self
     }
-    
-    func edgeGestureAction(sender: UIScreenEdgePanGestureRecognizer) {
-        switch sender.state {
-        case .began:
-            print("began")
-            
-            // if top most VC in stack is MusicSearchVC
-                // pop MusicSearchVC off of stack
-            // else if currentVC is CameraCaptureVC && user selected the music button
-                // performSegue(withIdentifier: showMusicSearchViewContoller, sender: self)
-
-        case .cancelled:
-            print("cancelled")
-        case .changed:
-            print("changed")
-            
-            if let edgeGesture = edgeGesture{
-                interactionController.update(percentForEdgePan(gesture: edgeGesture))
-            }
-            
-        case .ended:
-            print("ended")
-            interactionController.finish()
-        case .failed:
-            print("failed")
-            interactionController.cancel()
-        case .possible:
-            print("possible")
-        }
-    }
-    
-    private func percentForEdgePan(gesture: UIScreenEdgePanGestureRecognizer) -> CGFloat {
-        return 0.0
-    }
-    
-//    //| ----------------------------------------------------------------------------
-//    //! Returns the offset of the pan gesture recognizer from the edge of the
-//    //! screen as a percentage of the transition container view's width or height.
-//    //! This is the percent completed for the interactive transition.
-//    //
-//    - (CGFloat)percentForGesture:(UIScreenEdgePanGestureRecognizer *)gesture
-//    {
-//    // Because view controllers will be sliding on and off screen as part
-//    // of the animation, we want to base our calculations in the coordinate
-//    // space of the view that will not be moving: the containerView of the
-//    // transition context.
-//    UIView *transitionContainerView = self.transitionContext.containerView;
-//    
-//    CGPoint locationInSourceView = [gesture locationInView:transitionContainerView];
-//    
-//    // Figure out what percentage we've gone.
-//    
-//    CGFloat width = CGRectGetWidth(transitionContainerView.bounds);
-//    CGFloat height = CGRectGetHeight(transitionContainerView.bounds);
-//    
-//    // Return an appropriate percentage based on which edge we're dragging
-//    // from.
-//    if (self.edge == UIRectEdgeRight)
-//    return (width - locationInSourceView.x) / width;
-//    else if (self.edge == UIRectEdgeLeft)
-//    return locationInSourceView.x / width;
-//    else if (self.edge == UIRectEdgeBottom)
-//    return (height - locationInSourceView.y) / height;
-//    else if (self.edge == UIRectEdgeTop)
-//    return locationInSourceView.y / height;
-//    else
-//    return 0.f;
-//    }
-//
-    
 }
 
 extension CameraCaptureNavigationController: UINavigationControllerDelegate {
--- MusicMind/MusicMind/NavigateLeftTransitionInteractionController.swift
+++ MusicMind/MusicMind/NavigateLeftTransitionInteractionController.swift
@@ -10,5 +10,69 @@ import UIKit
 
 class NavigateLeftTransitionInteractionController: UIPercentDrivenInteractiveTransition {
     
-
+    func edgeGestureAction(sender: UIScreenEdgePanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            print("began")
+            
+            // if top most VC in stack is MusicSearchVC
+            // pop MusicSearchVC off of stack
+            // else if currentVC is CameraCaptureVC && user selected the music button
+            // performSegue(withIdentifier: showMusicSearchViewContoller, sender: self)
+            
+        case .cancelled:
+            print("cancelled")
+        case .changed:
+            print("changed")
+            
+            self.update(percentForEdgePan(gesture: sender))
+        case .ended:
+            print("ended")
+            self.finish()
+        case .failed:
+            print("failed")
+            self.cancel()
+        case .possible:
+            print("possible")
+        }
+    }
+    
+    private func percentForEdgePan(gesture: UIScreenEdgePanGestureRecognizer) -> CGFloat {
+        return 1.0
+    }
+    
+    //    //| ----------------------------------------------------------------------------
+    //    //! Returns the offset of the pan gesture recognizer from the edge of the
+    //    //! screen as a percentage of the transition container view's width or height.
+    //    //! This is the percent completed for the interactive transition.
+    //    //
+    //    - (CGFloat)percentForGesture:(UIScreenEdgePanGestureRecognizer *)gesture
+    //    {
+    //    // Because view controllers will be sliding on and off screen as part
+    //    // of the animation, we want to base our calculations in the coordinate
+    //    // space of the view that will not be moving: the containerView of the
+    //    // transition context.
+    //    UIView *transitionContainerView = self.transitionContext.containerView;
+    //
+    //    CGPoint locationInSourceView = [gesture locationInView:transitionContainerView];
+    //
+    //    // Figure out what percentage we've gone.
+    //
+    //    CGFloat width = CGRectGetWidth(transitionContainerView.bounds);
+    //    CGFloat height = CGRectGetHeight(transitionContainerView.bounds);
+    //
+    //    // Return an appropriate percentage based on which edge we're dragging
+    //    // from.
+    //    if (self.edge == UIRectEdgeRight)
+    //    return (width - locationInSourceView.x) / width;
+    //    else if (self.edge == UIRectEdgeLeft)
+    //    return locationInSourceView.x / width;
+    //    else if (self.edge == UIRectEdgeBottom)
+    //    return (height - locationInSourceView.y) / height;
+    //    else if (self.edge == UIRectEdgeTop)
+    //    return locationInSourceView.y / height;
+    //    else
+    //    return 0.f;
+    //    }
+    
 }
