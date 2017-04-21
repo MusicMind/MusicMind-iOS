807cb727e0a8d6f9acdce265003f4316596ef745
Wes Van der Klomp <wes.vdk@icloud.com>
2017-04-21 16:16:39 -0700

    spec out methods

--- MusicMind/MusicMind/UploadActivity.swift
+++ MusicMind/MusicMind/UploadActivity.swift
@@ -10,4 +10,27 @@ import UIKit
 
 class UploadActivity: UIActivity {
     
+    override var activityType: UIActivityType? {
+        return nil
+    }
+    
+    override var activityTitle: String? {
+        return "Upload to MusicMind"
+    }
+    
+    override var activityImage: UIImage? {
+        return #imageLiteral(resourceName: "cool")
+    }
+    
+    override func canPerform(withActivityItems activityItems: [Any]) -> Bool {
+        return true
+    }
+    
+    override func prepare(withActivityItems activityItems: [Any]) {
+        // code
+    }
+    
+//    override var activityCategory: UIActivityCategory {
+//        return UIActivityCategory.action
+//    }
 }
