5f05dd1175bdadf7836def6f3c190da203e1692f
Angel Contreras <angel.contreras23@me.com>
2017-04-24 12:03:14 -0700

    avvideoexporter class

---
+++ MusicMind/MusicMind/Stickers/AVVideoExporter.swift
@@ -0,0 +1,14 @@
+//
+//  AVVideoExporter.swift
+//  MusicMind
+//
+//  Created by Angel Contreras on 4/24/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class AVVideoExporter{
+    
+}
