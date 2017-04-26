e17fd8c1982fe5c6725ec33880d2da341dccfcef
Wes Van der Klomp <wes.vdk@icloud.com>
2017-04-26 16:44:48 -0700

    use UITableViewController and setup

--- MusicMind/MusicMind/UserSettingsViewController.swift
+++ MusicMind/MusicMind/UserSettingsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UserSettingsViewController: UIViewController {
+class UserSettingsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
