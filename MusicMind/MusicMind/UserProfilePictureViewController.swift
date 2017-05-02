0c60cfc815cf41aa2dc0590bbb88de0e007572aa
majickdave <majickdave@gmail.com>
2017-05-02 14:22:54 -0700

    added storyboard

---
+++ MusicMind/MusicMind/UserProfilePictureViewController.swift
@@ -0,0 +1,12 @@
+//
+//  UserProfilePictureViewController.swift
+//  MusicMind
+//
+//  Created by david samuel on 5/2/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
