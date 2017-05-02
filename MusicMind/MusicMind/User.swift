d3164245bb1865355c9fd658a1052d786a5453b7
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-02 14:20:50 -0700

    rm FirebaseDataService

--- MusicMind/MusicMind/User.swift
+++ MusicMind/MusicMind/User.swift
@@ -9,16 +9,33 @@ import Foundation
 
 struct User {
     
+    static let shared = FirebaseDataService()
+    
+    // DB References
+    private let userDatabase = database.child("users")
+    
+    func addUserToUserList(_ user: User) {
+        var userAsDictionary: [String: Any] = [:]
+        
+        for (key, value) in user.dictionaryRepresentation {
+            if let value = value {
+                userAsDictionary[key] = value
+            }
+        }
+        
+        userDatabase.child(user.firebaseUUID!).updateChildValues(userAsDictionary)
+    }
+    
+    fileprivate let database = FIRDatabase.database().reference()
+    fileprivate let storage = FIRStorage.storage().reference()
+
+    
+    var uuid: String?
     var firstName: String?
-    
     var lastName: String?
-    
+    var mobileNumber: String?
     var birthday: Date?
 //    let dateFormatter = DateFormatter()
 //    dateFormatter.dateFormat = "yyyy-MM-dd"
-    
-    var firebaseUUID: String?
-    
-    var mobileNumber: String?
     
 }
