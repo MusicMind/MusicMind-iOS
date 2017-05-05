6738f78adfa49111a55f8d13597325eb7732d6e5
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-05 16:59:55 -0700

    rm local scoped user

--- MusicMind/MusicMind/AppDelegate.swift
+++ MusicMind/MusicMind/AppDelegate.swift
@@ -14,8 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var user: User!
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         FIRApp.configure()
