c263a3c5f29fa3e1452c2f8934ada7aa5d57c166
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-08 10:50:51 -0700

    reworking pushNewUser

--- MusicMind/MusicMind/User.swift
+++ MusicMind/MusicMind/User.swift
@@ -62,7 +62,13 @@ class User {
         }
     }
     private(set) var email: String?
-    var profilePhoto: URL?
+    var profilePhoto: URL? {
+        didSet {
+            if let profilePhoto = profilePhoto, let userRef = userRef {
+                userRef.setValue(["profilePhoto": profilePhoto.absoluteString])
+            }
+        }
+    }
     
     init() {
         userRef = nil
@@ -76,30 +82,30 @@ class User {
     }
     
     func pushNewUserToFirebaseDatabase(assosiatedWithAuthUser authUser: FIRUser) {
-        userRef = FIRDatabase.database().reference().child("users/\(authUser.uid)")
-        
-        if let userRef = userRef,
-            let firstName = firstName,
-            let lastName = lastName,
-            let mobileNumber = mobileNumber,
-            let birthday = birthday {
-            
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd"
-            
-            let birthdayString = formatter.string(from: birthday)
-            
-            userRef.setValue(["firstName": firstName,
-                              "lastName": lastName,
-                              "mobileNumber": mobileNumber,
-                              "birthday": birthdayString],
-                             withCompletionBlock: { (error, ref) in
-                                // completion code
-            })
-            
-            
+        if let id = id {
+            userRef = FIRDatabase.database().reference().child("users\(id)")
+        } else {
+            userRef = FIRDatabase.database().reference().child("users").childByAutoId()
         }
         
+        if let userRef = userRef {
+            
+            // Construct a dictionary out of all non-nil properties then send to firebase
+            var dictionaryRepresentation: [String: String] = [:]
+            
+            if let birthday = birthday {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "yyyy-MM-dd"
+                
+                let birthdayString = formatter.string(from: birthday)
+                
+                dictionaryRepresentation["birthday"] = birthdayString
+            }
+
+            userRef.setValue(dictionaryRepresentation) { (error, ref) in
+                // completion code
+            }   
+        }
     }
     
     init(withAuthUser authUser: FIRUser) {
