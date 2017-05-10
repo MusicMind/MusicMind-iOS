61ca915fbdfa71fda1c022e557c43c55f47661e9
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-10 16:15:11 -0700

    remove inequality checks and cleanup

--- MusicMind/MusicMind/User.swift
+++ MusicMind/MusicMind/User.swift
@@ -45,76 +45,14 @@ struct User: FirebaseConvertable {
         firebaseAuthUser = FIRAuth.auth()?.currentUser
         id = firebaseAuthUser?.uid
         
-//        firstName = (snapshot.value as! [String: Any])["firstName"] as? String
-        
         if let userData = snapshot.value as? [String: Any] {
-
-            // First name
-            if let firstName = userData["firstName"] as? String {
-                if firstName != self.firstName {
-                    self.firstName = firstName
-                }
-            }
-
-            // Last name
-            if let lastName = userData["lastName"] as? String {
-                if lastName != self.lastName {
-                    self.lastName = lastName
-                }
-            }
-
-            // Mobile number
-            if let mobileNumber = userData["mobileNumber"] as? String {
-                if mobileNumber != self.mobileNumber {
-                    self.mobileNumber = mobileNumber
-                }
-            }
-
-            // Email
-            if let email = userData["email"] as? String {
-                if email != self.email {
-                    self.email = email
-                }
-            }
-
-            // Profile photo
-            if let profilePhotoUrlString = userData["profilePhoto"] as? String {
-                let profilePhotoUrl = URL(string: profilePhotoUrlString)
-
-                if let profilePhotoUrl = profilePhotoUrl {
-                    if profilePhotoUrl != self.profilePhoto {
-                        self.profilePhoto = profilePhotoUrl
-                    }
-                }
-            }
-
-            // Birthday
-            if let birthdayString = userData["birthday"] as? String {
-                let formatter = DateFormatter()
-                formatter.dateFormat = "yyyy-MM-dd"
-                
-                let birthdayDate = formatter.date(from: birthdayString)
-                
-                if let birthdayDate = birthdayDate {
-                    if birthdayDate != self.birthday {
-                        self.birthday = birthdayDate
-                    }
-                }
-            }
-            
-            // Date created
-            if let dateCreatedString = userData["dateCreated"] as? String {
-                let formatter = DateFormatter()
-                formatter.dateFormat = "yyyy-MM-dd"
-                
-                let dateCreatedDate = formatter.date(from: dateCreatedString)
-                
-                if let dateCreatedDate = dateCreatedDate {
-                    if dateCreatedDate != self.dateCreated {
-                        self.dateCreated = dateCreatedDate
-                    }
-                }
-            }
+            if let firstName = userData["firstName"] as? String { self.firstName = firstName }
+            if let lastName = userData["lastName"] as? String { self.lastName = lastName }
+            if let mobileNumber = userData["mobileNumber"] as? String { self.mobileNumber = mobileNumber }
+            if let email = userData["email"] as? String { self.email = email }
+            if let profilePhotoUrlString = userData["profilePhoto"] as? String { profilePhoto = URL(string: profilePhotoUrlString) }
+            if let birthdayString = userData["birthday"] as? String { birthday = dateFormatter.date(from: birthdayString) }
+            if let dateCreatedString = userData["dateCreated"] as? String { dateCreated = dateFormatter.date(from: dateCreatedString) }
         }
     }
     
