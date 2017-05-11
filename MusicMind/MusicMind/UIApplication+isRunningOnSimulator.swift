0ea8176087e3926f92cbb73eb93bad1f816a2013
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-11 14:13:06 -0700

    cleanup

--- MusicMind/MusicMind/UIApplication+isRunningOnSimulator.swift
+++ MusicMind/MusicMind/UIApplication+isRunningOnSimulator.swift
@@ -3,13 +3,13 @@
 //  MusicMind
 //
 //  Created by Wesley Van der Klomp on 5/11/17.
-//  Copyright © 2017 MusicMind. All rights reserved.
+//  https://gist.github.com/wvdk/e3dd57620c53f338978e6d5d022d3672
+//  Thanks to http://stackoverflow.com/a/30284266/6407050
 //
-
 import Foundation
 
 extension UIApplication {
- 
+    
     static var isRunningOnSimulator: Bool {
         return TARGET_OS_SIMULATOR != 0
     }
