250a369d4032386ad2186af24d0cef7b0104a60c
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-12 10:40:00 -0700

    add vc files

---
+++ MusicMind/MusicMind/FindFriendsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FindFriendsViewController.swift
+//  MusicMind
+//
+//  Created by Wesley Van der Klomp on 5/12/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+    
+}
