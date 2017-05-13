e934c161ed21977a507a7d4da4293ea273220747
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-12 17:44:26 -0700

    Add Firebase db maintenance functions file

---
+++ MusicMind/MusicMind/Firebase db maintenance functions.swift
@@ -0,0 +1,9 @@
+//
+//  Firebase db maintenance functions.swift
+//  MusicMind
+//
+//  Created by Wesley Van der Klomp on 5/12/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import Foundation
