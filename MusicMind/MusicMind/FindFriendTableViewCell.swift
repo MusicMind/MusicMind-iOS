14e6de319ff78c5417c8d301b42f7c5039be47cd
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-15 14:38:39 -0700

    label, outlet for img

---
+++ MusicMind/MusicMind/FindFriendTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FindFriendTableViewCell.swift
+//  MusicMind
+//
+//  Created by Wesley Van der Klomp on 5/15/17.
+//  Copyright © 2017 MusicMind. All rights reserved.
+//
+
+import UIKit
+
+class FindFriendsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    
+}
