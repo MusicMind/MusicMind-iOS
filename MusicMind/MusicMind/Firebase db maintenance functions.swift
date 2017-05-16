df4e27696ab9df6e33a60714cff29c08819fa353
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-16 11:38:45 -0700

    prep searchableNames generator

--- MusicMind/MusicMind/Firebase db maintenance functions.swift
+++ MusicMind/MusicMind/Firebase db maintenance functions.swift
@@ -9,44 +9,48 @@
 import Foundation
 import Firebase
 
-// Create a function that goes through every user and creates a “searchName” value
-//
-//func iterateThroughEveryUser() {
-//    
-//    
-//    
-//    let searchNames: [String: String]
-//    
-//    let usersRef = FIRDatabase.database().reference().child("users")
-//    
-//    usersRef.observe(.value) { (snapshot: FIRDataSnapshot) in
-//        
-//        
-//        let children = snapshot.children
-//        
-//        while let child = children.nextObject() as? FIRDataSnapshot {
-//            let user = User(withSnapshot: child)
-//            
-//            // create a SearchFriendResult
-//
-//            if let id = user.id {
-//                
-//                if let firstName = user.firstName {
-//                    
-//                }
-//                
-//                if let lastName = user.lastName {
-//                    
-//                }
-//                
-//                
-////                searchNames[""] = id
-//            }
-//        
-//            
-//            print(user.asDictionary)
-//        }
-//    }
-//}
-//
+/// Goal: To have "searchableNames" a lookup table of String 1->* UserID values
+/// Names must be lowercase, and concatenated first and last name
+/// Go through every user in the "users" table
+/// Generate searchableName value
+/// Add to "searchableName" table
+
+func iterateThroughEveryUser() {
+    
+    
+    
+    let searchNames: [String: String]
+    
+    let usersRef = FIRDatabase.database().reference().child("users")
+    
+    usersRef.observe(.value) { (snapshot: FIRDataSnapshot) in
+        
+        
+        let children = snapshot.children
+        
+        while let child = children.nextObject() as? FIRDataSnapshot {
+            let user = User(withSnapshot: child)
+            
+            // create a SearchFriendResult
+
+            if let id = user.id {
+                
+                if let firstName = user.firstName {
+                    
+                }
+                
+                if let lastName = user.lastName {
+                    
+                }
+                
+                
+//                searchNames[""] = id
+            }
+        
+            
+            print(user.asDictionary)
+        }
+    }
+}
+
 
