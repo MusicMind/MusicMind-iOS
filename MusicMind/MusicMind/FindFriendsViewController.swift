215c0142f92c7145c6381dd2a8d4a9d0043edbf2
Wes Van der Klomp <wes.vdk@icloud.com>
2017-05-19 17:51:33 -0700

    move some stuff into cell class

--- MusicMind/MusicMind/FindFriendsViewController.swift
+++ MusicMind/MusicMind/FindFriendsViewController.swift
@@ -26,25 +26,6 @@ class FindFriendsViewController: UIViewController {
         searchBar.delegate = self
         tableView.dataSource = self
         tableView.delegate = self
-        
-//        // Create a Firebase observer on the userFriends table which will update the TableView cells when a user is added or removed from the list
-//        if let currentUserId = FIRAuth.auth()?.currentUser?.uid {
-//            let usersFriendsRef = FIRDatabase.database().reference().child("userFriends/\(currentUserId)")
-//
-//            usersFriendsRef.observe(.value, with: { (snapshot: FIRDataSnapshot) in
-//                if let friends = snapshot.value as? [String: Bool] {
-//                    for (cellUserId, cell) in self.cellsByUser {
-//                        for (friendId, _) in friends {
-//                            if cellUserId == friendId {
-//                                cell.addButton.titleLabel?.text = "AAA"
-//                            } else {
-//                                cell.addButton.titleLabel?.text = "BBB"
-//                            }
-//                        }
-//                    }
-//                }
-//            })
-//        }
     }
     
     fileprivate func searchForUserByName(withString: String) {
--- MusicMind/MusicMind/FriendSearchResultController.swift
+++ MusicMind/MusicMind/FriendSearchResultController.swift
@@ -19,4 +19,29 @@ class FriendSearchResultController: UITableViewCell {
        
     }
     
+    //        // Create a Firebase observer on the userFriends table which will update the TableView cells when a user is added or removed from the list
+    //        if let currentUserId = FIRAuth.auth()?.currentUser?.uid {
+    //            let usersFriendsRef = FIRDatabase.database().reference().child("userFriends/\(currentUserId)")
+    //
+    //            usersFriendsRef.observe(.value, with: { (snapshot: FIRDataSnapshot) in
+    //                if let friends = snapshot.value as? [String: Bool] {
+    //                    for (cellUserId, cell) in self.cellsByUser {
+    //                        for (friendId, _) in friends {
+    //                            if cellUserId == friendId {
+    //                                cell.addButton.titleLabel?.text = "AAA"
+    //                            } else {
+    //                                cell.addButton.titleLabel?.text = "BBB"
+    //                            }
+    //                        }
+    //                    }
+    //                }
+    //            })
+    //        }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        
+    }
+    
 }
